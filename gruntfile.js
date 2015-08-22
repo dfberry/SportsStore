2f9f766d37166aa65c28a06ab1f669e5817ba1e4
Dina Berry <dinaberry@outlook.com>
2015-08-22 06:15:20 -0700

    grunt builds sass then starts web services

--- gruntfile.js
+++ gruntfile.js
@@ -4,16 +4,16 @@ module.exports = function(grunt) {
 		
 		pkg: grunt.file.readJSON("package.json"),
 
-			sass: {
-				options: {
-					sourceMap: true
-				},
-				dist: {
-					files: {
-						'static/scss/style.css': 'static/scss/style.scss'
-					}
+		sass: {
+			options: {
+				sourceMap: true
+			},
+			dist: {
+				files: {
+					'static/scss/style.css': 'static/scss/style.scss'
 				}
-			},		
+			}
+		},		
 		
 	
 		parallel: {
@@ -30,20 +30,12 @@ module.exports = function(grunt) {
 		
 	});
 	
-	// using spawn because I get a spawn error from grunt-run -- probably a path issue
-	//grunt.loadNpmTasks('grunt-spawn');
-	
-	// using to run node
-	//grunt.loadNpmTasks('grunt-run');
-
 	// run parallel tasks	
 	grunt.loadNpmTasks('grunt-parallel');
 	
 	// sass library
 	grunt.loadNpmTasks('grunt-sass');
 
-	
-	grunt.registerTask('default', ["transpose"]);
-	//grunt.registerTask('default', ["parallel"]);
+	grunt.registerTask('default', ["sass", "parallel"]);
 	
 };
