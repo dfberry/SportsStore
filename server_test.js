e29ceb80d70403e7a21de2a0f17a45c516378a24
Dina Berry <dinaberry@outlook.com>
2015-08-25 18:36:36 -0700

    [wip] still playing with modules

--- server_test.js
+++ server_test.js
@@ -1,3 +1,4 @@
 var moduleTest = require('./utils/module_test.js');
 
 console.log(moduleTest.test());
+console.log(moduleTest.add(5,9));
--- utils/module_test.js
+++ utils/module_test.js
@@ -1,4 +1,7 @@
 
 var five = function(){return 5;}
 
+var add = function(a,b){return a + b;}
+
 module.exports.test = five;
+module.exports.add = add;
