773022aac8841dbecdf0a542b372eb201412331e
Dina Berry <dinaberry@outlook.com>
2015-08-31 08:58:58 -0700

    exception and info work outside of mocha

--- test/non-automated-tests/logger_test.js
+++ test/non-automated-tests/logger_test.js
@@ -2,3 +2,5 @@ var logger = require("../../utils/logger.js")
 
 logger.write('non-automated test');
 var myexception = nonexistant();
+
+//console.log(logger);
--- utils/logger.js
+++ utils/logger.js
@@ -1,76 +1,26 @@
 // log using different transports or my own logging
 var winston = require('winston');
-var fileSystem = require('fs');
-var fileStreamRotator = require('file-stream-rotator');
+var logger = new winston.Logger();
 
-// always the log dir
-var DEFAULTLOGDIRECTORY = process.cwd() + '/log';
+// from github winstonjs readme
+// the logger also emits an 'error' event which you should 
+// handle or suppress if you don't want unhandled exceptions
+//winston.on('error', function (err) { /* Do Something */ });
+logger.emitErrs = false;
 
-// custom log dir
-var logDirectory;
+// informational log
+logger.add(winston.transports.DailyRotateFile, { level: 'info', filename: './log/winston.log' });
 
-winston.emitErrs = true;
+// exception log
+logger.handleExceptions(new winston.transports.DailyRotateFile({ filename: './log/winston_exceptions.log',handleExceptions: true }));
 
-// create a rotating write stream 
-//var accessLogStream = fileStreamRotator.getStream({
-//  filename: logDirectory + '/%DATE%.log',
-//  frequency: 'daily',
-//  verbose: false
-//});
+// internal functions
+var write = function(message,encoding){
+    logger.log('info',message);
+}
 
-
-var logger = new winston.Logger({
-    transports: [
-        new winston.transports.DailyRotateFile({
-            level: 'debug',
-            filename: logDirectory + '/log',
-            datePattern: '.yyyy-MM-ddTHH',
-            handleExceptions: true,
-            json: true,
-            maxsize: 5242880, //5MB
-            maxFiles: 5,
-            colorize: false
-        }),
-        new winston.transports.Console({
-            level: 'debug',
-            handleExceptions: true,
-            json: false,
-            colorize: true
-        })
-    ],
-    exitOnError: false
-});
-
-
-
-var write =  function(message, encoding){
-        logger.info(message);
-};
-
-// init logger
-var init = function(dir){
-    
-    console.log('logDirectory=' + logDirectory);
-
-    // if another directory is passed in for the log file, 
-    // use it, otherwise use default log file path    
-    if (dirExists(dir))
-        logDirectory = dir; 
-    else
-        logDirectory = DEFAULTLOGDIRECTORY;
-    
-    return logDirectory;
-};
-
-// ensure log directory exists 
-var dirExists = function(dir){
-    
-    return fileSystem.existsSync(dir) || fileSystem.mkdirSync(dir);  
-};
-
+// exported functions
 module.exports = {
-    stream: write,
-    init: init,
-    dirExists: dirExists
+    write: write
 };
 
