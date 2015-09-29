259ac51a2f2f62b764f9390f7a780752a1523a13
dfberry <dfberry@dfberrys-MacBook-Air.local>
2015-09-29 14:08:20 -0700

    bootstrap ddl

--- public/app/Controllers/GeozipController.js
+++ public/app/Controllers/GeozipController.js
@@ -4,7 +4,9 @@ app.controller('GeozipController',
     
     $scope.test = "testing,1,2,3";
     $scope.fields = [];
+    $scope.searchField="";
     $scope.searchResults = [];
+    $scope.searchTerm;
     
     var getFields = function (){
         GeozipService.fields(function (data) {
@@ -17,6 +19,10 @@ app.controller('GeozipController',
                 console.log(data);
                 $scope.searchResults = data;
             });
+    };
+    $scope.setSearchField = function(newSearchField){
+        console.log("newSearchField = " + newSearchField);
+        $scope.searchField = newSearchField;
     };
     
     /*
@@ -40,56 +46,7 @@ app.controller('GeozipController',
       getFields();  
     };
     
+
+    
     init();
 }]);
-/*
-var Geozip;
-(function (Geozip) {
-    console.log('GeozipController.js::beginning of function');
-
-    'use strict';
-    var GeozipController = (function () {
-        
-        console.log('GeozipController.js::controller definition begins');
-        
-        function GeozipController($scope, geozipService) {
-            console.log('GeozipController.js::controller definition - constructor');
-            this.scope = $scope;
-            this.geozipService = geozipService;
-            this.fields();
-        }
-        GeozipController.prototype.fields = function () {
-            console.log('GeozipController.js::controller definition - fields');
-
-            var _this = this;
-            this.geozipService.fields(function (data) {
-                console.log('GeozipController.js::controller definition - fields - results:');
-                console.log(data);
-                console.log(data[0]);
-                _this.scope['fields'] = data[0];
-            });
-        };
-        GeozipController.prototype.search = function (searchfield, searchterm) {
-            console.log('GeozipController.js::controller definition - search');
-            
-            var _this = this;
-            this.geozipService.search(searchfield, searchterm, function (data) {
-                _this.scope['search'] = data;
-            });
-        };
-        console.log('GeozipController.js::controller definition ends');
-
-        return GeozipController;
-    })();
-    Geozip.getModule().controller('geozipController', [
-        '$scope',
-        'geozipService',
-        function ($scope, geozipService) { 
-            console.log('GeozipController- return new controller');
-            return new GeozipController($scope, geozipService); 
-        }
-    ]);
-    console.log('GeozipController.js::end of function');
-
-})(Geozip || (Geozip = {}));
-*/
